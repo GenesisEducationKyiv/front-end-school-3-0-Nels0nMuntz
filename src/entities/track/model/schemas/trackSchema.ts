cbfccbb3bbd2ec2f78b6ccf3d3d753de9460f0da
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-06-03 22:09:13 +0300

    fix trackSchema to allow empty string for coverImage field

--- src/entities/track/model/schemas/trackSchema.ts
+++ src/entities/track/model/schemas/trackSchema.ts
@@ -7,7 +7,7 @@ export const trackSchema = z.object({
   album: z.string().optional(),
   genres: z.array(z.string()),
   slug: z.string(),
-  coverImage: z.string().url().optional(),
+  coverImage: z.string().url().optional().or(z.literal("")),
   audioFile: z.string().optional(),
   createdAt: z.string().datetime(),
   updatedAt: z.string().datetime(),
