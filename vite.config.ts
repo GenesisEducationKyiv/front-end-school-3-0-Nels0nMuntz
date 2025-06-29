b16fbec623d7847dd61d25948c292ad4691be04d
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-06-30 02:32:22 +0300

    update sourcemap settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    analyzer({
-      openAnalyzer: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const enableSourceMap = env.VITE_ENABLE_SOURCEMAP === "true";
+  
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      analyzer({
+        openAnalyzer: false,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    sourcemap: false,
-  },
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build: {
+      sourcemap: enableSourceMap,
+    },
+  };
 });
